ff13a20796107831f1e1564331ab1af70c2a9ac1
Haicheng Li <haicheng.li@outlook.com>
2023-01-30 19:57:41 +0800

    sh: let qemu use alternative tcp port for gdb debugging
    Signed-off-by: Haicheng Li <haicheng.li@outlook.com>

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ set -x
 #qemu-system-riscv32 -bios none -kernel hc.bin -s -S
 #qemu-system-riscv64 -smp 4 -machine virt -bios none -kernel hc.elf -s -S -serial pty
 #qemu-system-x86_64 -smp 4 -M q35 -kernel build/hypervisor/acrn.out -s -S -serial pty
-qemu-system-x86_64 -smp 4 -kernel build/hypervisor/acrn.32.out -s -S -serial pty -machine kernel-irqchip=split -cpu Denverton -m 1G,slots=3,maxmem=4G
+#qemu-system-x86_64 -smp 4 -kernel build/hypervisor/acrn.32.out -s -S -serial pty -machine kernel-irqchip=split -cpu Denverton -m 1G,slots=3,maxmem=4G
+qemu-system-x86_64 -smp 4 -kernel build/hypervisor/acrn.32.out -gdb tcp::1235 -S -serial pty -machine kernel-irqchip=split -cpu Denverton -m 1G,slots=3,maxmem=4G
 
 #qemu-system-x86_64 -smp 4 -kernel build/hypervisor/acrn.32.out -s -S -serial pty -machine kernel-irqchip=split -cpu Denverton,+invtsc,+lm,+nx,+smep,+smap,+mtrr,+clflushopt,+vmx,+x2apic,+popcnt,-xsave,+sse,+rdrand,-vmx-apicv-vid,+vmx-apicv-xapic,+vmx-apicv-x2apic,+vmx-flexpriority,+tsc-deadline,+pdpe1gb -m 1G,slots=3,maxmem=4G
 
